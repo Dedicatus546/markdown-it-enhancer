a71b372b157a28750090197652b392069c0c031c
Par9uet <par9uet@qq.com>
2025-10-16 15:51:25 +0800

    refactor(mdurl): 去除 console

--- packages/md-url/test/format.test.ts
+++ packages/md-url/test/format.test.ts
@@ -6,7 +6,6 @@ import fixtures from "./fixtures/url";
 describe("format", () => {
   Object.keys(fixtures).forEach((url) => {
     it(url, () => {
-      console.log(url, parse(url));
       const parsed = parse(url);
       expect(format(parsed)).toBe(url);
     });
